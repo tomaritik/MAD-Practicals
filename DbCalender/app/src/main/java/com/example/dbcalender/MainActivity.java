0cb3d4631309a0d140d8767d6402d12010a75ee0
tomaritik <84390766+tomaritik@users.noreply.github.com>
2021-06-01 15:26:14 +0530

    Add files via upload

---
+++ DbCalender/app/src/main/java/com/example/dbcalender/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.dbcalender;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private mySQLiteDBHandler dbHandler;
+    private EditText editText;
+    private CalendarView calendarView;
+    private String selectedDate;
+    private SQLiteDatabase sqLiteDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        calendarView = findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                selectedDate = Integer.toString(year) + Integer.toString(month) + Integer.toString(dayOfMonth);
+                ReadDatabase(view);
+            }
+        });
+
+        try{
+
+            dbHandler = new mySQLiteDBHandler(this, "CalendarDatabase", null,1);
+            sqLiteDatabase = dbHandler.getWritableDatabase();
+            sqLiteDatabase.execSQL("CREATE TABLE EventCalendar(Date TEXT, Event TEXT)");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void InsertDatabase(View view){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("Date",selectedDate);
+        contentValues.put("Event", editText.getText().toString());
+        sqLiteDatabase.insert("EventCalendar", null, contentValues);
+
+    }
+
+    public void ReadDatabase(View view){
+        String query = "Select Event from EventCalendar where Date = " + selectedDate;
+        try{
+            Cursor cursor = sqLiteDatabase.rawQuery(query, null);
+            cursor.moveToFirst();
+            editText.setText(cursor.getString(0));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            editText.setText("");
+        }
+    }
+
+}
+package com.example.dbcalender;
+
+        import android.content.Context;
+        import android.database.sqlite.SQLiteDatabase;
+        import android.database.sqlite.SQLiteOpenHelper;
+        import android.support.annotation.Nullable;
+
+public class mySQLiteDBHandler extends SQLiteOpenHelper {
+    public mySQLiteDBHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
